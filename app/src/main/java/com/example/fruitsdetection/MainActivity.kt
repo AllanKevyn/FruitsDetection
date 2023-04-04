c263298c52615c40aea636e31e3494ac943a1c92
allankevyn <aksilvasouza@hotmail.com>
2023-04-03 22:00:23 -0300

    new implementation test

--- app/src/main/java/com/example/fruitsdetection/MainActivity.kt
+++ app/src/main/java/com/example/fruitsdetection/MainActivity.kt
@@ -1,108 +1,238 @@
 package com.example.fruitsdetection
 
-import android.app.Activity
-import android.content.ContentValues
-import android.content.Intent
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.net.Uri
-import android.os.Build
+import android.graphics.Canvas
+import android.graphics.SurfaceTexture
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraManager
 import android.os.Bundle
-import android.provider.MediaStore
-import android.view.View
+import android.os.Handler
+import android.os.HandlerThread
+import android.view.Surface
+import android.view.TextureView
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import java.io.FileDescriptor
-import java.io.IOException
+import com.example.fruitsdetection.ml.SsdMobilenetV11Metadata1
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.ResizeOp
 
 class MainActivity : AppCompatActivity() {
-    var frame: ImageView? = null
+
+
+    lateinit var imageProcessor: ImageProcessor
+    lateinit var bitmap: Bitmap
+    lateinit var imageView: ImageView
+    lateinit var cameraDevice: CameraDevice
+    lateinit var handler: Handler
+    private lateinit var cameraManager: CameraManager
+    private lateinit var textureView: TextureView
+    lateinit var model: SsdMobilenetV11Metadata1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        frame = findViewById(R.id.imageView)
+        textureView = findViewById(R.id.textureView)
+        getPermission()
 
+        imageProcessor = ImageProcessor.Builder().add(ResizeOp(300, 300, ResizeOp.ResizeMethod.BILINEAR)).build()
 
-        //TODO ask for permission of camera upon first launch of application
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkSelfPermission(android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED || checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                == PackageManager.PERMISSION_DENIED) {
-                val permission = arrayOf(android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                requestPermissions(permission, 112)
-            }
-        }
+        model = SsdMobilenetV11Metadata1.newInstance(this)
+        val handlerThread = HandlerThread("videoThread")
+        handlerThread.start()
+        handler = Handler(handlerThread.looper)
 
-        //TODO captue image using camera
-        frame?.setOnLongClickListener(View.OnLongClickListener {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if (checkSelfPermission(android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED || checkSelfPermission(
-                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE
-                    )
-                    == PackageManager.PERMISSION_DENIED
-                ) {
-                    val permission = arrayOf(
-                        android.Manifest.permission.CAMERA,
-                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE
-                    )
-                    requestPermissions(permission, 121)
-                } else {
-                    openCamera()
-                }
-            } else {
+        imageView = findViewById(R.id.imageView)
+
+        textureView.surfaceTextureListener = object:TextureView.SurfaceTextureListener{
+            override fun onSurfaceTextureAvailable(p0: SurfaceTexture, p1: Int, p2: Int) {
+
                 openCamera()
+
             }
-            true
-        })
 
-        //TODO chose image from gallery
-        frame?.setOnClickListener(View.OnClickListener {
-            val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-            startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE)
-        })
+            override fun onSurfaceTextureSizeChanged(p0: SurfaceTexture, p1: Int, p2: Int) {
+            }
+
+            override fun onSurfaceTextureDestroyed(p0: SurfaceTexture): Boolean {
+                return false
+            }
+
+            override fun onSurfaceTextureUpdated(p0: SurfaceTexture) {
+                bitmap= textureView.bitmap!!
+                var image = TensorImage.fromBitmap(bitmap)
+                image = imageProcessor.process(image)
+
+                val outputs = model.process(image)
+                val locations = outputs.locationsAsTensorBuffer
+                val classes = outputs.classesAsTensorBuffer
+                val scores = outputs.scoresAsTensorBuffer
+                val numberOfDetections = outputs.numberOfDetectionsAsTensorBuffer
+
+                var mutableBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+                val canvas = Canvas(mutableBitmap)
+
+            }
+
+        }
+        cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+
+
+
     }
 
-    var image_uri: Uri? = null
-    private val RESULT_LOAD_IMAGE = 123
-    val IMAGE_CAPTURE_CODE = 654
+    override fun onDestroy() {
+        super.onDestroy()
+        model.close()
+    }
 
-    //TODO opens camera so that user can capture image
+    @SuppressLint("MissingPermission")
     private fun openCamera() {
-        val values = ContentValues()
-        values.put(MediaStore.Images.Media.TITLE, "New Picture")
-        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera")
-        image_uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
-        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri)
-        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE)
+        cameraManager.openCamera(cameraManager.cameraIdList[0], object:CameraDevice.StateCallback(){
+            override fun onOpened(p0: CameraDevice) {
+
+                cameraDevice = p0
+
+                var surfaceTexture = textureView.surfaceTexture
+                var surface = Surface(surfaceTexture)
+
+                var captureRequest = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+                captureRequest.addTarget(surface)
+
+                cameraDevice.createCaptureSession(listOf(surface), object :CameraCaptureSession.StateCallback(){
+                    override fun onConfigured(p0: CameraCaptureSession) {
+                        p0.setRepeatingRequest(captureRequest.build(), null, null)
+                    }
+
+                    override fun onConfigureFailed(p0: CameraCaptureSession) {
+
+                    }
+
+
+                }, handler)
+            }
+
+            override fun onDisconnected(p0: CameraDevice) {
+
+            }
+
+            override fun onError(p0: CameraDevice, p1: Int) {
+
+            }
+
+        }, handler)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == IMAGE_CAPTURE_CODE && resultCode == Activity.RESULT_OK) {
-            //imageView.setImageURI(image_uri);
-            val bitmap = uriToBitmap(image_uri!!)
-            frame?.setImageBitmap(bitmap)
-        }
-        if (requestCode == RESULT_LOAD_IMAGE && resultCode == Activity.RESULT_OK && data != null) {
-            image_uri = data.data
-            //imageView.setImageURI(image_uri);
-            val bitmap = uriToBitmap(image_uri!!)
-            frame?.setImageBitmap(bitmap)
+    private fun getPermission() {
+        if (checkSelfPermission(android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED || checkSelfPermission(
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            == PackageManager.PERMISSION_DENIED
+        ) {
+            val permission = arrayOf(
+                android.Manifest.permission.CAMERA,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            requestPermissions(permission, 101)
         }
     }
 
-    //TODO takes URI of the image and returns bitmap
-    private fun uriToBitmap(selectedFileUri: Uri): Bitmap? {
-        try {
-            val parcelFileDescriptor = contentResolver.openFileDescriptor(selectedFileUri, "r")
-            val fileDescriptor: FileDescriptor = parcelFileDescriptor!!.fileDescriptor
-            val image = BitmapFactory.decodeFileDescriptor(fileDescriptor)
-            parcelFileDescriptor.close()
-            return image
-        } catch (e: IOException) {
-            e.printStackTrace()
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (grantResults[0] !=  PackageManager.PERMISSION_GRANTED){
+            getPermission()
         }
-        return null
     }
+
+
+//        //TODO ask for permission of camera upon first launch of application
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            if (checkSelfPermission(android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED || checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+//                == PackageManager.PERMISSION_DENIED) {
+//                val permission = arrayOf(android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+//                requestPermissions(permission, 112)
+//            }
+//        }
+//
+//        //TODO captue image using camera
+//        frame?.setOnLongClickListener(View.OnLongClickListener {
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//                if (checkSelfPermission(android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED || checkSelfPermission(
+//                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+//                    )
+//                    == PackageManager.PERMISSION_DENIED
+//                ) {
+//                    val permission = arrayOf(
+//                        android.Manifest.permission.CAMERA,
+//                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+//                    )
+//                    requestPermissions(permission, 121)
+//                } else {
+//                    openCamera()
+//                }
+//            } else {
+//                openCamera()
+//            }
+//            true
+//        })
+//
+//        //TODO chose image from gallery
+//        frame?.setOnClickListener(View.OnClickListener {
+//            val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+//            startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE)
+//        })
+//    }
+//
+//    var image_uri: Uri? = null
+//    private val RESULT_LOAD_IMAGE = 123
+//    val IMAGE_CAPTURE_CODE = 654
+//
+//    //TODO opens camera so that user can capture image
+//    private fun openCamera() {
+//        val values = ContentValues()
+//        values.put(MediaStore.Images.Media.TITLE, "New Picture")
+//        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera")
+//        image_uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+//        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri)
+//        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE)
+//    }
+//
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        if (requestCode == IMAGE_CAPTURE_CODE && resultCode == Activity.RESULT_OK) {
+//            //imageView.setImageURI(image_uri);
+//            val bitmap = uriToBitmap(image_uri!!)
+//            frame?.setImageBitmap(bitmap)
+//        }
+//        if (requestCode == RESULT_LOAD_IMAGE && resultCode == Activity.RESULT_OK && data != null) {
+//            image_uri = data.data
+//            //imageView.setImageURI(image_uri);
+//            val bitmap = uriToBitmap(image_uri!!)
+//            frame?.setImageBitmap(bitmap)
+//        }
+//    }
+//
+//    //TODO takes URI of the image and returns bitmap
+//    private fun uriToBitmap(selectedFileUri: Uri): Bitmap? {
+//        try {
+//            val parcelFileDescriptor = contentResolver.openFileDescriptor(selectedFileUri, "r")
+//            val fileDescriptor: FileDescriptor = parcelFileDescriptor!!.fileDescriptor
+//            val image = BitmapFactory.decodeFileDescriptor(fileDescriptor)
+//            parcelFileDescriptor.close()
+//            return image
+//        } catch (e: IOException) {
+//            e.printStackTrace()
+//        }
+//        return null
+//    }
 }
